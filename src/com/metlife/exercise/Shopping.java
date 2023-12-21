e5e94bb842cb86c54bd384c2c98eab379c1fdfa7
UndrajavarapuVijayKumar <vijaykumar.chinnu5@gmail.com>
2023-12-21 17:45:36 +0530

    first commit

--- src/com/metlife/exercise/Shopping.java
+++ src/com/metlife/exercise/Shopping.java
@@ -14,11 +14,13 @@ public class Shopping {
         System.out.println(i1.getDescr());
         i1.setQuantity(5);
         i1.setPrice(100000);
-        
+
         i1.DiscountPrice();
         i2.setDescr("laptops");
         i2.setQuantity(2);
         i2.setPrice(10000);
         i2.DiscountPrice();
+
+        //continution in git
     }
 }
